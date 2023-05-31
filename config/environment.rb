321290510583f7959ef379eb616ddb5694117a74
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-05-31 09:39:57 +0300

    completed lab

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,21 @@
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
+
+
+
+class Environment
+    require_relative '../lib/config/garden'
+    require_relative '../lib/plant'
+    
+    lawn = Garden.new(name: 'Front Lawn')
+    
+    basil = Plant.new(name: 'Basil')
+    basil.garden = lawn
+    
+    cucumber = Plant.new(name: 'Cucumber')
+    cucumber.garden = lawn
+    
+    p lawn.plants
+    
+     end
